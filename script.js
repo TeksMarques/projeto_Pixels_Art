0f0dd8fab0c3e449cf4c7b98b763ff0096668373
Teresa Marques <taotaodoce@gmail.com>
2022-07-04 16:25:15 -0300

    corrigindo

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-let quadros = document.getElementById('pixel-board')
+const quadros = document.getElementById('pixel-board')
 
 function criandoQuadrados(n) {
-    for (let index =0; index < n; index +=1){
-        let conjunto = document.createElement('div')
+    for (let index = 0; index < n; index += 1) {
+        const conjunto = document.createElement('div')
         conjunto.className = 'linha';
-        for(let i =1; i <= n; i +=1){
-            let quadrado = document.createElement('div')
+        for (let i = 1; i <= n; i += 1) {
+            const quadrado = document.createElement('div')
             quadrado.className = 'pixel';
             conjunto.appendChild(quadrado)
         }
