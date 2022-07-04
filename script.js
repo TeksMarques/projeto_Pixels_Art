9f9bca9d09b04425cf4bfa7f86aefdff7720e6f0
Teresa Marques <taotaodoce@gmail.com>
2022-07-04 16:30:41 -0300

    corrigindo alguns erros de pontuação

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-
-const quadros = document.getElementById('pixel-board')
+const quadros = document.getElementById('pixel-board');
 
 function criandoQuadrados(n) {
-    for (let index = 0; index < n; index += 1) {
-        const conjunto = document.createElement('div')
-        conjunto.className = 'linha';
-        for (let i = 1; i <= n; i += 1) {
-            const quadrado = document.createElement('div')
-            quadrado.className = 'pixel';
-            conjunto.appendChild(quadrado)
-        }
-        quadros.appendChild(conjunto)
+  for (let index = 0; index < n; index += 1) {
+    const conjunto = document.createElement('div');
+    conjunto.className = 'linha';
+    for (let i = 1; i <= n; i += 1) {
+      const quadrado = document.createElement('div');
+      quadrado.className = 'pixel';
+      conjunto.appendChild(quadrado);
     }
+    quadros.appendChild(conjunto);
+  }
 }
 
-criandoQuadrados(5)
+criandoQuadrados(5);
